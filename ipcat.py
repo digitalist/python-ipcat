825f9bb7f72bb82bedc6f3c7a6f73b5a1da3a0d2
Alexey Elymanov <strangeqargo@gmail.com>
2018-06-26 14:51:58 +0300

    python-3 support

--- ipcat.py
+++ ipcat.py
@@ -12,13 +12,13 @@ LAST_UPDATE = None
 SQLITE_CURSOR = None
 DOWNLOAD_FRESH_IPCAT_DELTA_DAYS = 4
 IPCAT_HOME = os.path.join(os.path.expandvars(os.path.expanduser("~")), ".ipcat")
-IPCAT_FILENAME = os.path.join(IPCAT_HOME, "ipcat.csv")
-IPCAT_SQLITE = os.path.join(IPCAT_HOME, "ipcat.sqlite")
+IPCAT_FILENAME = os.path.join(IPCAT_HOME, "datacenters.csv")
+IPCAT_SQLITE = os.path.join(IPCAT_HOME, "datacenters.sqlite")
 IPCAT_URL = "https://raw.github.com/client9/ipcat/master/datacenters.csv"
 
 def _addr_to_int(value):
     _ = value.split('.')
-    return (long(_[0]) << 24) + (long(_[1]) << 16) + (long(_[2]) << 8) + long(_[3])
+    return (int(_[0]) << 24) + (int(_[1]) << 16) + (int(_[2]) << 8) + int(_[3])
 
 def _retrieve_url(url, filename=None):
     try:
@@ -64,7 +64,7 @@ def _update():
                 cur.execute("CREATE TABLE ranges (start_int INT, end_int INT, name TEXT)")
 
                 with open(IPCAT_FILENAME) as f:
-                    for row in f.xreadlines():
+                    for row in f.readlines():
                         if not row.startswith('#') and not row.startswith('start'):
                             row = row.strip().split(",")
                             cur.execute("INSERT INTO ranges VALUES (?, ?, ?)", (_addr_to_int(row[0]), _addr_to_int(row[1]), row[2]))
@@ -74,8 +74,8 @@ def _update():
 
         LAST_UPDATE = time.time()
 
-    except Exception, ex:
-        print("[x] something went wrong during database update ('%s')" % str(ex), file=sys.stderr)
+    except Exception as e:
+        print("[x] something went wrong during database update ('%s')" % str(e), file=sys.stderr)
 
 def lookup(address):
     global SQLITE_CURSOR
